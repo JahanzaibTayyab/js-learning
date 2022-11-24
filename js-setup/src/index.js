a3bf24f70e4b2e4212ab0e12f04290767b4791d0
Jahanzaib Tayyab <jahanzaib.tayyab@devigital.com>
2022-11-24 22:12:59 +0500

    add descripation about regular exp

--- js-setup/src/index.js
+++ js-setup/src/index.js
@@ -1,6 +1,8 @@
 import inquirer from "inquirer";
 
 const validatePhoneNumber = (value) => {
+  // regular function
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
   const pass = value.match(
     /^([01]{1})?[-.\s]?\(?(\d{3})\)?[-.\s]?(\d{3})[-.\s]?(\d{4})\s?((?:#|ext\.?\s?|x\.?\s?){1}(?:\d+)?)?$/i
   );
@@ -9,6 +11,11 @@ const validatePhoneNumber = (value) => {
   }
   return "Please enter a valid phone number";
 };
+
+//promsie .then((value))
+// const result = await promise
+
+// async await
 
 const takeUserInput = async () => {
   const result = await inquirer.prompt([
@@ -47,5 +54,34 @@ const takeUserInput = async () => {
 
 const welcome = () => {
   takeUserInput();
+  //   inquirer
+  //     .prompt([
+  //       {
+  //         type: "input",
+  //         name: "first_name",
+  //         message: "What's your first name",
+  //       },
+  //       {
+  //         type: "input",
+  //         name: "last_name",
+  //         message: "What's your last name",
+  //       },
+  //       {
+  //         type: "input",
+  //         name: "hobby",
+  //         message: "What's your hobby",
+  //       },
+  //       {
+  //         type: "input",
+  //         name: "fav_color",
+  //         message: "What's your fav color",
+  //         default() {
+  //           return "Black";
+  //         },
+  //       },
+  //     ])
+  //     .then((value) => {
+  //       console.log(value);
+  //     });
 };
 welcome();
