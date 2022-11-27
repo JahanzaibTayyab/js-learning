90bf68b03fac31a5c03ca48240a72cf853804478
Jahanzaib Tayyab <jahanzaib.tayyab@devigital.com>
2022-11-27 10:02:15 +0500

    lec-26 nov

---
+++ 26-Nov/src/learn.js
@@ -0,0 +1,22 @@
+console.log("Hello");
+
+let a = 123;
+console.log("🚀 ~ file: index.js ~ line 4 ~ a", a);
+a = "Hello i am a";
+console.log("🚀 ~ file: index.js ~ line 6 ~ a", a);
+
+const add = (num1, num2) => {
+  return num1 + num2;
+};
+
+// typescript allows developers to add types
+
+// 3 types
+// number
+//string
+//boolean
+
+// implicit  type// guess value based on assigned value to variable
+// e.g let ses=123
+// explicit type // strong typing
+// e.g let exp:string="hello i am string"
